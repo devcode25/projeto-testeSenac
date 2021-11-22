2d0e0417965739cfe2f7ce80d174a897b3e5bd3a
devcode25 <93724227+devcode25@users.noreply.github.com>
2021-11-22 16:43:19 -0300

    Update routes.js

--- src/routes/empresasTecRoutes.js
+++ src/routes/empresasTecRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controller/empresasTecController')
 
-router.get("/", controller.getAll);
-router.get("/empresas", controller.getAll);
+router.get("/", controller.getAllEmpresas);
+
 
 module.exports = router;
