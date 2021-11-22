b69291c2333f316f8264c1c15443fed3280014a4
devcode25 <93724227+devcode25@users.noreply.github.com>
2021-11-22 16:42:04 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send({
+router.get('/', function(req, res) {
+    res.status(200).send({
         titulo:'Empresas tec legais para trampar',
-        data: "26/09/2020"
+        data: '26/09/2020'
     }) 
 })
 
-module.exports = router;
+module.exports = router; 
