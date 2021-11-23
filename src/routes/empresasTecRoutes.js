ca85e184e3e12de46f5cffa8a1b2d48ae6003f3f
devcode25 <93724227+devcode25@users.noreply.github.com>
2021-11-23 08:55:52 -0300

    Update routes create and get by id

--- src/routes/empresasTecRoutes.js
+++ src/routes/empresasTecRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const controller = require('../controller/empresasTecController')
 
 router.get("/", controller.getAllEmpresas);
+router.post("/",  controller.createEmpresas);
+router.get("/:id", controller.getEmpresas);
 
 
 module.exports = router;
