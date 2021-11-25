94cb7f753bab2d2ffc187f25df3aad029e85fb12
devcode25 <93724227+devcode25@users.noreply.github.com>
2021-11-25 16:29:05 -0300

    Criei a rota Put

--- src/controller/empresasTecController.js
+++ src/controller/empresasTecController.js
@@ -33,9 +33,40 @@ const getEmpresas = (req, res) => {
 
 
 
+const updateEmpresas = (req, res) => {
+    try {
+        const empresasId = req.params.id
+        const empresasToUpdate = req.body //Pego o corpo da requisição com as alterações 
+
+        const empresasFound = empresas.find(empresas => empresas.id == empresasId) // separo o item que irei atualizar      
+        const empresasIndex = empresas.indexOf(empresasFound) // separo o indice do item no array de empresas
+
+        if (empresasIndex >= 0) { // verifico se o dado existe no array de filmes
+            empresas.splice(empresasIndex, 1, empresasToUpdate) //busco no array o dado, excluo o registro antigo e substituo pelo novo 
+        } else {
+            res.status(404).send({ message: "Filme não encontrado para ser atualizado" })
+        }
+
+        fs.writeFile("./src/models/empresasTec.json", JSON.stringify(empresas), 'utf8', function (err) { // gravo meu json de empresas atualizado
+            if (err) {
+                res.status(500).send({ message: err }) // caso dê erro retorno status 500
+            } else {
+                console.log("Arquivo de filmes atualizado com sucesso!")
+                const empresasUpdated = empresas.find(empresas => empresas.id == empresasId) // separo o dado que modifiquei no array
+                res.status(200).send(empresasUpdated) // envio o dado modificado como resposta
+            }
+        })
+    } catch (err) {
+        res.status(500).send({ message: err }) // caso dê erro retorno status 500
+    }
+}
+
+
 module.exports = {
+    updateEmpresas,
     getEmpresas,
     createEmpresas,
     getAllEmpresas
 }
+
 
